dde9d0941349b949ad5cda98361c6ca0dc395c05
Javier Andrade <javier01123@gmail.com>
2022-03-13 11:05:47 -0600

    api prefixed paths

--- books_api/books_api/urls.py
+++ books_api/books_api/urls.py
@@ -24,8 +24,7 @@ router.register(r'authors', views.AuthorViewSet, basename='authors')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(router.urls)),
+    path('api/',include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
-
-# urlpatterns += router.urls
+ 
