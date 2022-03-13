1a440fe39300613f85cdec96d84737c565cbe36b
Javier Andrade <javier01123@gmail.com>
2022-03-13 11:19:08 -0600

    fix admin author registration

--- books_api/api/models.py
+++ books_api/api/models.py
@@ -5,8 +5,14 @@ class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return  '{} {}'.format(self.first_name,self.last_name)
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     subtitle = models.CharField(max_length=50)
     description = models.TextField()
     authors = models.ManyToManyField(Author)
+
+    def __str__(self) -> str:
+        return self.title
--- books_api/books_api/admin_models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from api.models import Author
-
-@admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
-    pass
